01349a0d66c958e4cae9466c9fa9f2981517e029
Kwok369 <kwok369@sina.cn>
2017-09-06 11:29:28 +0800

    update Test_SocketRequest
    InputStream → InputStreamReader → BufferedReader .
    Why：
    InputStream.available() ：  Note that while some implementations of
    InputStream will return the total number of bytes in the stream, many
    will not. It is never correct to use the return value of this method to
    allocate a buffer intended to hold all data in this stream.

--- src/testlib/socket/Test_SocketRequest.java
+++ src/testlib/socket/Test_SocketRequest.java
@@ -1,8 +1,10 @@
 package testlib.socket;
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.net.Socket;
 
@@ -33,13 +35,23 @@ public class Test_SocketRequest {
 		bw.write("\r\n");
 		bw.flush();
 		
+		socket.shutdownOutput(); //禁用此套接字的输出流。必须使用！！！否则 SocketServer 一致等待输入流结束，而抛超时异常。
+		
 		Thread.sleep(50);
-				
+		
+		/*		
 		InputStream is = socket.getInputStream();
 		byte[] b=new byte[is.available()];
 		is.read(b);
 		
 		System.out.println(new String(b));
+		*/
+		
+		BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+		String line;
+		while ((line = in.readLine()) != null) {
+			System.out.println(line);
+	    }
 		
 		socket.close();
 		
@@ -62,14 +74,24 @@ public class Test_SocketRequest {
         bw.write(paramString);
         bw.flush();
         
+        socket.shutdownOutput(); //禁用此套接字的输出流。必须使用！！！否则 SocketServer 一致等待输入流结束，而抛超时异常。
+        
         Thread.sleep(50);
         
+        /*
         InputStream is=socket.getInputStream();
         byte[] bytes=new byte[is.available()];
         is.read(bytes);
         System.out.println(new String(bytes));
+        */
+        
+        BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+		String line;
+		while ((line = in.readLine()) != null) {
+			System.out.println(line);
+	    }
         
         socket.close();
-        
+		
     }
 }
