e9a52debd40e0cb8483c7dea928e7530adda6d1c
Kwok <AndyKuo@yeah.net>
2021-07-18 18:52:25 +0800

    Add Java Security Apache Shiro Test.
    1.add Test_JavaSecurity\src\test\java\org\kwok\shiro\Test_Shiro.java .
    2.add Test_JavaSecurity\src\test\java\org\kwok\shiro\shiro.ini .
    3.add Test_JavaSecurity\src\main\resources\log4j.properties .
    4.update Test_JavaSecurity\pom.xml .

---
+++ Test_JavaSecurity/src/test/java/org/kwok/shiro/Test_Shiro.java
@@ -0,0 +1,91 @@
+package org.kwok.shiro;
+
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.IncorrectCredentialsException;
+import org.apache.shiro.authc.LockedAccountException;
+import org.apache.shiro.authc.UnknownAccountException;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.mgt.DefaultSecurityManager;
+import org.apache.shiro.realm.text.IniRealm;
+import org.apache.shiro.session.Session;
+import org.apache.shiro.subject.Subject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Test_Shiro {
+
+	private static final transient Logger log = LoggerFactory.getLogger(Test_Shiro.class);
+
+	public static void main(String[] args) {
+		
+		DefaultSecurityManager securityManager=new DefaultSecurityManager();
+		IniRealm iniRealm=new IniRealm("classpath:org/kwok/shiro/shiro.ini");
+		securityManager.setRealm(iniRealm);
+
+        SecurityUtils.setSecurityManager(securityManager);
+		
+		
+		Subject currentUser = SecurityUtils.getSubject();
+		
+		Session session = currentUser.getSession();
+        session.setAttribute("someKey", "aValue");
+        String value = (String) session.getAttribute("someKey");
+        if (value.equals("aValue")) {
+            log.info("Retrieved the correct value! [" + value + "]");
+        }
+
+        // let's login the current user so we can check against roles and permissions:
+        if (!currentUser.isAuthenticated()) {
+            UsernamePasswordToken token = new UsernamePasswordToken("lonestarr", "vespa");
+            token.setRememberMe(true);
+            try {
+                currentUser.login(token);
+            } catch (UnknownAccountException uae) {
+                log.info("There is no user with username of " + token.getPrincipal());
+            } catch (IncorrectCredentialsException ice) {
+                log.info("Password for account " + token.getPrincipal() + " was incorrect!");
+            } catch (LockedAccountException lae) {
+                log.info("The account for username " + token.getPrincipal() + " is locked.  " +
+                        "Please contact your administrator to unlock it.");
+            }
+            // ... catch more exceptions here (maybe custom ones specific to your application?
+            catch (AuthenticationException ae) {
+                //unexpected condition?  error?
+            }
+        }
+
+        //say who they are:
+        //print their identifying principal (in this case, a username):
+        log.info("User [" + currentUser.getPrincipal() + "] logged in successfully.");
+
+        //test a role:
+        if (currentUser.hasRole("schwartz")) {
+            log.info("May the Schwartz be with you!");
+        } else {
+            log.info("Hello, mere mortal.");
+        }
+
+        //test a typed permission (not instance-level)
+        if (currentUser.isPermitted("lightsaber:wield")) {
+            log.info("You may use a lightsaber ring.  Use it wisely.");
+        } else {
+            log.info("Sorry, lightsaber rings are for schwartz masters only.");
+        }
+
+        //a (very powerful) Instance Level permission:
+        if (currentUser.isPermitted("winnebago:drive:eagle5")) {
+            log.info("You are permitted to 'drive' the winnebago with license plate (id) 'eagle5'.  " +
+                    "Here are the keys - have fun!");
+        } else {
+            log.info("Sorry, you aren't allowed to drive the 'eagle5' winnebago!");
+        }
+
+        //all done - log out!
+        currentUser.logout();
+
+        System.exit(0);
+		
+	}
+
+}
