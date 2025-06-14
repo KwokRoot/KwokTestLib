68448d5e6fb20f681d1ca139c6d9793358c5bdb8
Kwok <AndyKuo@yeah.net>
2025-06-14 21:06:36 +0800

    Fix Bug About JPython Cases .
    1.update Test_JavaToolLib\src\main\java\org\kwok\jython\Test_Python.java .
    2.update Test_JavaToolLib\src\main\java\org\kwok\jython\Test_JPython.java .
    3.update Test_JavaToolLib\src\main\resources\jython\test_jpython.py .

--- Test_JavaToolLib/src/main/java/org/kwok/jython/Test_JPython.java
+++ Test_JavaToolLib/src/main/java/org/kwok/jython/Test_JPython.java
@@ -12,7 +12,7 @@ public class Test_JPython {
         
         //执行 Python 语句、Python 文件
         interpreter.exec("a = 'helloworld.'");
-        interpreter.execfile(Test_JPython.class.getResource("test_python.py").getPath());
+        interpreter.execfile(Thread.currentThread().getContextClassLoader().getResourceAsStream("jython/test_jpython.py"));
 		
         //关闭 Python 解释器
         interpreter.close();
--- Test_JavaToolLib/src/main/java/org/kwok/jython/Test_Python.java
+++ Test_JavaToolLib/src/main/java/org/kwok/jython/Test_Python.java
@@ -1,8 +1,8 @@
 package org.kwok.jython;
 
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.stream.Stream;
 
 public class Test_Python {
 
@@ -11,23 +11,23 @@ public class Test_Python {
 		String[] cmd = new String[] { "python", "-V"};
 		try {
 			/*
-			 * ProcessBuilder processBuilder = new ProcessBuilder(cmd); 
+			 * ProcessBuilder processBuilder = new ProcessBuilder(cmd);
 			 * Process process = processBuilder.start();
 			 */
 			Process process = Runtime.getRuntime().exec(cmd);
 
 			BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
-			String line;
-			while ((line = reader.readLine()) != null) {
-				System.out.println(line);
-			}
+			BufferedReader reader_error = new BufferedReader(new InputStreamReader(process.getErrorStream()));
+			Stream.concat(reader.lines(), reader_error.lines()).forEach(
+					line -> {
+						System.out.println(line);
+					}
+			);
 
 			int exitCode = process.waitFor();
 			System.out.println("return " + exitCode);
 
-		} catch (IOException e) {
-			e.printStackTrace();
-		} catch (InterruptedException e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 
