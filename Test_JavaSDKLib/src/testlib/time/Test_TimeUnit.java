6ede5991f047993aaa1078344bec1d9bba41978e
Kwok <AndyKuo@yeah.net>
2025-06-21 21:53:47 +0800

    Update Test_TimeUnit.java

--- Test_JavaSDKLib/src/testlib/time/Test_TimeUnit.java
+++ Test_JavaSDKLib/src/testlib/time/Test_TimeUnit.java
@@ -13,11 +13,16 @@ public class Test_TimeUnit {
 		
 		// 小时 转 毫秒
 		System.out.println(TimeUnit.HOURS.toMillis(1));
+		System.out.println(TimeUnit.MILLISECONDS.convert(1, TimeUnit.HOURS));
+		
 		// 天 转 毫秒
 		System.out.println(TimeUnit.DAYS.toMillis(1));
+		System.out.println(TimeUnit.MILLISECONDS.convert(1, TimeUnit.DAYS));
+		
 		// 毫秒 转 天
 		System.out.println(TimeUnit.MILLISECONDS.toDays(86400000));
-
+		System.out.println(TimeUnit.DAYS.convert(86400000, TimeUnit.MILLISECONDS));
+		
 	}
 
 }
